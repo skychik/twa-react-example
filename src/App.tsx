18ba8afd2d27bf6791ecfea7e17faa1226a57981
Kirill <kir-esakov2011@mail.ru>
2023-01-21 12:42:04 +0400

    feat: show theme.backgroundColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import { useSDK } from '@twa.js/sdk-react';
+import { useSDK, useThemeParams, useWebApp } from '@twa.js/sdk-react';
 
 function App() {
-  const sdk = useSDK();
   const [count, setCount] = useState(0);
+
+  const webApp = useWebApp();
+  const sdk = useSDK();  
+  const theme = useThemeParams();
+
+  useEffect(() => {
+    webApp.ready();
+  }, [webApp]);
 
   return (
     <div className="App">
@@ -22,9 +29,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <div>Background color: {theme.backgroundColor}</div>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
